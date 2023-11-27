34593ff51b901367329e9ef5b9c0b7d1c0a7f644
Hoang Phuong Le <lephuonghoang198@gmail.com>
2023-11-27 16:55:06 +0700

    feat(examples typescript): do examples about enum in typescript

---
+++ examples/enum.ts
@@ -0,0 +1,9 @@
+enum ProgrammingLanguages {
+    Javascript = 'JAVASCRIPT',
+    C = 'C',
+    Python = 'Python',
+    Java = 'Java'
+  }
+
+console.log(ProgrammingLanguages.Java);
+console.log(ProgrammingLanguages['Javascript']);
