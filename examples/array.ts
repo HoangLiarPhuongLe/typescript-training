ace9f954a1804ffef1fde4bb93aaf44a6acc092b
Hoang Phuong Le <lephuonghoang198@gmail.com>
2023-11-27 16:52:34 +0700

    feat(examples typescript): do examples about array in typescript

---
+++ examples/array.ts
@@ -0,0 +1,9 @@
+var numbers:number[] = [1,2,3,3];
+console.log(numbers[0]); 
+console.log(numbers[1]); 
+console.log(numbers[2]); 
+console.log(numbers[3]); 
+
+var strings:Array<string> = ["Le", "Hoang", "Duc", "Liem"];
+console.log(strings[0]);
+console.log(strings[1]);
